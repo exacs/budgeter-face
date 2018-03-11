c71ac3d023263cd8376b9799cd9329e9ae232dbb
Carlos Saito <c.saito.style@gmail.com>
2018-03-11 15:43:18 +0100

    Add routes for e-mail login...
    Add the following routes
    - GET /private
    - GET /login
    - POST /login
    - GET /confirm

--- app/index.js
+++ app/index.js
@@ -2,7 +2,25 @@
  * The Web Application written in Koa
  */
 const Koa = require('koa');
+const _ = require('koa-route');
 const app = new Koa();
+
+app.use(_.get('/private', async ctx => {
+  ctx.body = 'GET /private';
+}));
+
+app.use(_.get('/login', async ctx => {
+  ctx.body = 'GET /login';
+}));
+
+app.use(_.post('/login', async ctx => {
+  ctx.body = 'POST /login';
+}));
+
+app.use(_.get('/confirm', async ctx => {
+  ctx.body = 'GET /confirm';
+}));
+
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
