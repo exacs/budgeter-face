5fad1268adb0248879c89c29feafc23346984d93
Carlos Saito <c.saito.style@gmail.com>
2018-03-25 16:15:38 +0200

    Add the "POST /login" request from <LoginForm />

--- client/scenes/login-form.js
+++ client/scenes/login-form.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 
 import EmailForm from '../components/email-form';
+
+const ASK_EMAIL = 0;
+const WAIT_CODE = 1;
+const SUCCESS = 2;
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      phase: ASK_EMAIL
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(email) {
-    console.log('Submit!!!', email);
+    const options = {
+      method: 'post',
+      body: {
+        email
+      },
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    fetch('/login', options)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({phase: WAIT_CODE});
+      });
   }
 
   render() {
-    return <EmailForm onSubmit={this.handleSubmit} />;
+    const component = [
+      <EmailForm onSubmit={this.handleSubmit} />,
+      <div>We have sent you an e-mail with a magic link. Open it to login</div>,
+      <div>Success!</div>
+    ][this.state.phase];
+
+    return <div>{component}</div>;
   }
 }
 
