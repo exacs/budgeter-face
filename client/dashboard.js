799b748ee9a031fc7b10f98e54a560bbf21895e5
Carlos Saito <c.saito.style@gmail.com>
2018-03-26 23:55:51 +0200

    Add <Dashboard /> components

---
+++ client/dashboard.js
@@ -0,0 +1,9 @@
+/**
+ * File to be served in "GET /login"
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Dashboard from './scenes/dashboard';
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
---
+++ client/scenes/dashboard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const dashboard = () => <div>This is a dashboard</div>;
+
+export default dashboard;
