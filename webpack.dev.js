1577d1a68f8a5914618f29668183e72af7ea8a22
Carlos Saito <c.saito.style@gmail.com>
2018-03-27 22:54:07 +0200

    Add React HMR

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,17 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
 const whm = 'webpack-hot-middleware/client';
 
 module.exports = merge(common, {
   mode: 'development',
 
   entry: {
-    login: [whm, common.entry.login],
-    dashboard: [whm, common.entry.dashboard],
-  }
+    login: [common.entry.login],
+    dashboard: [common.entry.dashboard],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 });
